b4fc15c1619e9f6e85018829150522b4686cdb8e
abbas.m <zaz@zeze.com>
2021-09-30 00:32:40 +0200

    ajout abbréviation de string

--- src/main/java/com/mab/helloworld2/HelloWorld.java
+++ src/main/java/com/mab/helloworld2/HelloWorld.java
@@ -1,9 +1,15 @@
 package com.mab.helloworld2;
 
+import org.apache.commons.lang3.StringUtils;
+
 public class HelloWorld {
 
     public static void main(String[] args) {
         System.out.println("Hello world 2");
+        String phraseUnPeuLongue = "Cette phrase et un peu beaucoup longue et elle ne tiendra pas dans l'écran";
+        System.out.println(phraseUnPeuLongue);
+        String phraseRaccourcie = StringUtils.abbreviate(phraseUnPeuLongue, 24);
+        System.out.println(phraseRaccourcie);
     }
 
 }
